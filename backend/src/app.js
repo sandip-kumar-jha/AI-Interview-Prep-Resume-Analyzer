a66fad4d3b261cff008eec753f6ba61e3c8fded9
sandip kumar <sandipjha654@gmail.com>
2026-08-13 12:58:44 +0530

    Fix production CORS configuration

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,25 +1,28 @@
-const express = require("express")
-const cookieParser = require("cookie-parser")
-const cors = require("cors")
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cookieParser())
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
-}))
+app.use(
+    cors({
+        origin: [
+            "http://localhost:5173",
+            "https://cute-cheesecake-74835c.netlify.app",
+        ],
+        credentials: true,
+    })
+);
+
+app.use(express.json());
+app.use(cookieParser());
 
 /* require all the routes here */
-const authRouter = require("./routes/auth.routes")
-const interviewRouter = require("./routes/interview.routes")
-
+const authRouter = require("./routes/auth.routes");
+const interviewRouter = require("./routes/interview.routes");
 
 /* using all the routes here */
-app.use("/api/auth", authRouter)
-app.use("/api/interview", interviewRouter)
+app.use("/api/auth", authRouter);
+app.use("/api/interview", interviewRouter);
 
-
-
-module.exports = app
+module.exports = app;
